1a06cce465aba23a6c4efb4b69a439aee27523a3
Asad Parvez <asadparvez17@gmail.com>
2015-05-08 13:02:26 +1000

    Small Adjustment to menu

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,14 @@
 function menu(){
 	var menu = document.getElementById("menu_wrap");
+	var body = document.getElementsByTagName("body")[0];
 	if(menu.getAttribute("active") == "0"){
 		menu.style.right = "0px";
+		body.style.marginLeft = "-200px";
 		menu.setAttribute("active", "1");
 	}
 	else if(menu.getAttribute("active") == "1"){
 		menu.style.right = "-300px";
+		body.style.marginLeft = "0px";
 		menu.setAttribute("active", "0");
 	}
 }
