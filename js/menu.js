bf292178f9e97a5250b6bb228423581c6e99f885
Asad Parvez <asadparvez17@gmail.com>
2015-05-10 22:24:11 +1000

    Individual Hotspot Page
    Site design is for the most part completed.

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,12 @@ function menu(){
 	var body = document.getElementsByTagName("body")[0];
 	if(menu.getAttribute("active") == "0"){
 		menu.style.right = "0px";
-		body.style.marginLeft = "-200px";
+		body.style.left = "-200px";
 		menu.setAttribute("active", "1");
 	}
 	else if(menu.getAttribute("active") == "1"){
 		menu.style.right = "-300px";
-		body.style.marginLeft = "0px";
+		body.style.left = "0px";
 		menu.setAttribute("active", "0");
 	}
 }
