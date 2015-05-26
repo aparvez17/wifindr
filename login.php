55ab09a0a2e876c34eff157e33c70f2fd0ed1a0c
dannyiss <dannyiss@gmail.com>
2015-05-26 23:44:39 +1000

    Some Comments

--- login.php
+++ login.php
@@ -3,23 +3,28 @@ session_start();
 require("dbconnect.inc");
 
 if (isset($_POST["username"]) and isset($_POST["password"])){
-		//3.1.1 Assigning posted values to variables.
+		//Assign Variables
 		$username = $_POST['username'];
 		$password = md5($_POST['password']);
-		//3.1.2 Checking the values are existing in the database or not
+		//Query Database and get a counter of how many results, in theory 0 or 1 is the only result
 		$userdata = $pdo->prepare("SELECT count(*) FROM `user` WHERE email='$username' and password='$password'");
 		$userdata->execute();
 		$foundRows = $userdata->fetchColumn();
-		//3.1.2 If the posted values are equal to the database values, then session will be created for the user.
+		//If there is 1 you then set the session 
 	if ($foundRows == 1){
-	$_SESSION["username"] = $username;
+		$userdata2 = $pdo->prepare("SELECT * FROM `user` WHERE email='$username' and password='$password'");
+		$userdata2->execute();
+		$result = $userdata2->fetch(PDO::FETCH_OBJ);
+		$_SESSION["id"] = $result->ID;
+		$_SESSION["name"] = $result->USERNAME;
+		$_SESSION["username"] = $username;
 	
 	}else{
-	//3.1.3 If the login credentials doesn"t match, he will be shown with an error message.
+	//If the login credentials doesn't match
 	echo 'Invalid Login Credentials';
 	}
 	}
-	//3.1.4 if the user is logged in Greets the user with message
+	//Sets the header if there is a user
 	if (isset($_SESSION["username"])){
 		header("Location: index.php");
 	}
