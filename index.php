cecfa2f9f1c4b48f54b05dddb7b64bd348bfe43d
Asad Parvez <asadparvez17@gmail.com>
2015-05-26 22:41:04 +1000

    Commented and included index and results

--- index.php
+++ index.php
@@ -1,67 +1,66 @@
 <?php
 include 'dbconnect.inc';
 include 'login.php';
-try{
-	$suburbs = $pdo->query('SELECT DISTINCT suburb from hotspots');
-}
-catch (PDOException $e){
-	echo $e->getMessage();
-} 
-
-$sub_list = Array();
-foreach ($suburbs as $suburb){
-	$clean_suburb = trim(preg_replace('/[0-9,]+/', '', $suburb['suburb']));
-	array_push($sub_list, $clean_suburb);
-}
-$sub_list = array_unique($sub_list);
+include 'includes/index.inc';
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Social Media Meta Data -->
 		<meta name="description" content="Welcome to wiFindr! wiFindr lets you discover and rate free wi-fi hotspots.">
 		<meta property="og:title" content="wiFindr - Join the Free Wi-Fi Revolution">
         <meta property="og:image" content="images/logo/logo_black.png">
         <meta property="og:description" content="wiFindr lets you discover and rate free wi-fi hotspots.">
 
+        <!-- Mobile Meta Data  -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        	<!-- iOS  -->
         <link rel="apple-touch-icon-precomposed" href="images/icons/icon_mobile.png"/>
         <link rel="apple-touch-startup-image" href="images/splash.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
+        	<!-- Android  -->
         <link rel="manifest" href="manifest.json">
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" sizes="192x192" href="images/icons/icon_mobile.png">
 
 		<title>wiFindr - Find free wi-fi near you</title>
+
+		<!-- Stylesheets  -->
 		<link rel="stylesheet" href="css/reset.css" type="text/css">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<link rel="stylesheet" href="css/animation.css" type="text/css">
 		<link rel="stylesheet" href="css/mobile.css" type="text/css">
+
+		<!-- Online Fonts  -->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,400' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-		<script type="text/javascript" 
-		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfHOKpEYRngpFqOITq0YIIm_puZYbPEs4">
-		</script>	
+
+		<!-- External Javascript Files  -->
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfHOKpEYRngpFqOITq0YIIm_puZYbPEs4"></script>	
 		<script type="text/javascript" src="js/geo.js"></script>
+		<script type="text/javascript" src="js/map.js"></script>
+		<script type="text/javascript" src="js/menu.js"></script>
 	</head>
+
+	<!-- LoadMap Function generates map and places markers onload -->
 	<body onload="loadMap(-27.471110, 153.022686)">
 		<div class="wrapper">
+
+			<!-- Navigation Menu -->
 			<nav id="menu_wrap" class="menu-closed" active="0">
 				<a href="javascript:void(0);" id="close_menu" class="left" onclick="menu()"><img src="images/icons/arrow.png" alt="Close Menu"/></a>
 				<h3 class="center-text padding30">MENU</h3>
 				<ul>
-				<?php 
-		        	echo $_SESSION['username'];
-		        	if (isset($_SESSION['username'])){
-		        		echo "<li>Learn More</li>";
-		        	}
-		        		 
+					<?php 
+			        	echo $_SESSION["username"];
+			        	if (isset($_SESSION['username'])){
+			        		echo "<li>Learn More</li>";
+			        	} 
 		        	?>
-					
 					<li onclick="login()">Log in</li>
 					<div id="login" open="0">
 						<form action="login.php" method="POST">
@@ -76,13 +75,16 @@ $sub_list = array_unique($sub_list);
 					<li>Terms</li>
 				</ul>
 			</nav>
+
+			<!-- Main Content -->
 			<div id="content">
 				<div class="page_wrap">		
 					<div class="page center">
-
 						<div id="promo_headline" class="center">
 							<h1 class="center-text">We know you're only here for the free Wi-Fi</h1>
 						</div>
+
+						<!-- Search Box -->
 						<div id="searchbox" class="page">
 							<img id="index_logo" class="left" src="images/logo/logo_black.png" alt="wiFindr - Find free wi-fi near you" />
 							<form action="results.php" method="POST" name="search">
@@ -109,14 +111,16 @@ $sub_list = array_unique($sub_list);
 						</div>
 					</div>
 				</div>
+
+				<!-- Header -->
 				<div id="header_wrap">
 					<div id="header" class="center">
 						<a href="javascript:void(0);" id="menu_icon" class="right" onclick="menu()"><img src="images/icons/menu_icon.png" alt="Open Menu"/></a>
 					</div>
 				</div>
-				
-			</div>
-			
+			</div
+
+			<!-- Footer -->
 			<div id="footer_wrap">
 				<div id="footer" class="page center">
 					<img class="left" src="images/logo/footer_icon.png" alt="wiFindr" />
@@ -125,8 +129,5 @@ $sub_list = array_unique($sub_list);
 			</div>
 		</div>
 		<div id="map-canvas" class="map-canvas"></div>
-
-		<script type="text/javascript" src="js/map.js"></script>
-		<script type="text/javascript" src="js/menu.js"></script>
 	</body>
 </html>
--- results.php
+++ results.php
@@ -1,104 +1,63 @@
 <?php
 include 'dbconnect.inc';
 include 'login.php';
-$selected_suburb = $_POST['select_suburb'];
-$sent_rating = $_POST['sort_rating'];
-$usr_latitude = $_POST['latitude'];
-$usr_longitude = $_POST['longitude'];
-
-$search_message = "";
-$case = 0;
-
-if($usr_latitude != ""){
-	$hotspots = $pdo->prepare('SELECT * FROM hotspots');
-	$hotspots->execute();
-	$latitude = $usr_latitude;
-	$longitude = $usr_longitude;
-
-	$hot_count = $hotspots->rowCount();
-	$id_array = Array();
-	foreach($hotspots as $hotspot){
-		$distance = sqrt(pow(($hotspot['latitude']-$usr_latitude), 2)+pow(($hotspot['longitude']-$usr_longitude), 2));
-		if ($distance < 0.015){
-			$hotspots = array_push($id_array, $hotspot['id']);
-		}
-	}
-
-	$hotspots = $pdo->prepare('SELECT * FROM hotspots WHERE id IN ('.implode(',', array_map('intval', $id_array)).')');
-	$hotspots->execute();
-	$case = 1;
-}
-else{
-	if($selected_suburb != ""){
-		if($sent_rating != ""){
-			$hotspots = $pdo->prepare('SELECT * FROM hotspots WHERE suburb LIKE :searched AND hotspot_rating > :sent_rating');
-			$hotspots->bindValue(':searched', "%".$selected_suburb."%");
-			$hotspots->bindValue(':sent_rating', $sent_rating);
-			$case = 2;		
-		}
-		else{
-			$hotspots = $pdo->prepare('SELECT * FROM hotspots WHERE suburb LIKE :searched');
-			$hotspots->bindValue(':searched', "%".$selected_suburb."%");
-			
-			$case = 3;	
-		}
-	}
-	else{
-		if($sent_rating != ""){
-			$hotspots = $pdo->prepare('SELECT * FROM hotspots WHERE hotspot_rating > :sent_rating');
-			$hotspots->bindValue(':sent_rating', $sent_rating);
-			$case = 4;
-		}
-		else{
-			$hotspots = $pdo->prepare('SELECT * FROM hotspots');
-			$case = 5;
-		}
-	}
-	$hotspots->execute();
-
-	$results = $hotspots->fetch(PDO::FETCH_ASSOC);
-	$longitude = $results['longitude'];
-	$latitude = $results['latitude'];
-	$hotspots->execute();
-
-}
-
-$hotspots->execute();
-$hot_count = $hotspots->rowCount();
-
-
+include 'includes/results.inc';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+
+		<!-- Social Media Meta Data -->
 		<meta name="description" content="Browse hotspots for your specified location.">
 		<meta property="og:title" content="wiFindr - Join the Free Wi-Fi Revolution">
         <meta property="og:image" content="images/logo/logo_black.png">
         <meta property="og:description" content="wiFindr lets you discover and rate free wi-fi hotspots.">
-		<title>wiFindr - Results for 
+		<title>wiFindr - Results 
 			<?php if($selected_suburb != ""){
-				echo $selected_suburb;
+				echo "for",$selected_suburb;
 				}
 				else{
-					echo "your location";
+					echo "for your location";
 				}
-				 ?></title>
+			?>
+		</title>
+
+		<!-- Mobile Meta Data  -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        	<!-- iOS  -->
+        <link rel="apple-touch-icon-precomposed" href="images/icons/icon_mobile.png"/>
+        <link rel="apple-touch-startup-image" href="images/splash.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+        	<!-- Android  -->
+        <link rel="manifest" href="manifest.json">
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="icon" sizes="192x192" href="images/icons/icon_mobile.png">
+
+        <!-- Stylesheets  -->
 		<link rel="stylesheet" href="css/reset.css" type="text/css">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<link rel="stylesheet" href="css/animation.css" type="text/css">
 		<link rel="stylesheet" href="css/mobile.css" type="text/css">
 		
+		<!-- Online Fonts  -->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,400' rel='stylesheet' type='text/css'>
-		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+		<!-- External Javascript Files  -->
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 		<script type="text/javascript" src="js/map.js"></script>
-    
-		
+		<script type="text/javascript" src="js/menu.js"></script>
 	</head>
+
 	<body onload="loadMap(<?php echo $latitude ?>, <?php echo $longitude?>)">
 		<div id="wrapper">
+
+			<!-- Navigation Menu -->
 			<nav id="menu_wrap" class="menu-closed" active="0">
 				<a href="javascript:void(0);" id="close_menu" class="left" onclick="menu()"><img src="images/icons/arrow.png" alt="Close Menu"/></a>
 				<h3 class="center-text padding30">MENU</h3>
@@ -117,12 +76,16 @@ $hot_count = $hotspots->rowCount();
 					<li>Terms</li>
 				</ul>
 			</nav>
+
+			<!-- Header -->
 			<div id="header_wrap" class="yellow_bg">
 	            <div id="header" class="center">
 	                <a href="index.php"><img src="images/logo/logo_white.png" class="left header-logo" alt="wiFindr"></a>
 	                <a href="javascript:void(0);" id="menu_icon" class="right" onclick="menu()"><img src="images/icons/menu_icon.png" alt="Open Menu"/></a>
 	            </div>
 	        </div>
+
+	        <!-- Main Content -->
 	        <div id="content">
 		        <div id="map-canvas" class="map-canvas-short"></div>
 		        <div class="page center padding30">
@@ -132,6 +95,8 @@ $hot_count = $hotspots->rowCount();
 			        	echo $search_message;
 		        		?>
 		        	</h2>
+
+		        	<!-- Dynamically Display Hotspots Results -->
 		        	<div id="results-wrap">
 		        		<?php
 		        			foreach($hotspots as $hotspot){
@@ -153,6 +118,8 @@ $hot_count = $hotspots->rowCount();
 		        	</div>
 		        </div>
 		    </div>
+
+		    <!-- Footer -->
 	       	<div id="footer_scroll_wrap">
 				<div id="footer" class="page center">
 					<img class="left" src="images/logo/footer_icon.png" alt="wiFindr" />
@@ -160,7 +127,5 @@ $hot_count = $hotspots->rowCount();
 				</div>
 			</div>
 	    </div>
-
-        <script type="text/javascript" src="js/menu.js"></script>
 	</body>
 </html>
